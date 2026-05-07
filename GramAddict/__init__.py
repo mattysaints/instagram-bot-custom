bcfab2ed44cd9c5e0f0eb7d17c63b80bd7b3e7b3
MARRA MATTIA <mattia.marra@esternibisp.com>
2026-05-07 21:18:56 +0200

    fix: bootstrap runtime env for PyCharm runs

--- GramAddict/__init__.py
+++ GramAddict/__init__.py
@@ -3,6 +3,10 @@
 __version__ = "3.2.12"
 __tested_ig_version__ = "300.0.0.29.110"
 
+from GramAddict.runtime_env import bootstrap_runtime_env
+
+bootstrap_runtime_env()
+
 from GramAddict.core.bot_flow import start_bot
 
 
